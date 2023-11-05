fd7fed5c7a817830f247a3c4f559f7384733b7a7
Reyansh Kumar <109780452+Reyansh-Kumar-Programmer@users.noreply.github.com>
2023-11-05 11:02:22 +0530

    Basic Setup fo the App Signup and LoginPage Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />}/>
+
+        {/* private routes */}
+        <Route index element={<Home />}/>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
