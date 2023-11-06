46d2125addd665b2ac0f9625b1857819f3bde718
Reyansh Kumar <109780452+Reyansh-Kumar-Programmer@users.noreply.github.com>
2023-11-06 21:49:55 +0530

    Starting to design the Sign up Page 👌

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import AuthLayout from "./_auth/AuthLayout";
 import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
@@ -8,10 +11,15 @@ const App = () => {
     <main className="flex h-screen">
       <Routes>
         {/* public routes */}
-        <Route path="/sign-in" element={<SigninForm />}/>
-
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />}/>
+          <Route path="/sign-up" element={<SignupForm />}/>
+        </Route>
+        
         {/* private routes */}
-        <Route index element={<Home />}/>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}/>
+        </Route>
       </Routes>
     </main>
   );
